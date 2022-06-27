6fe45cf74ad3394414432c657e0b34e53b30bd22
Michelle Stone <michellechristine.s91@gmail.com>
2022-06-27 00:58:36 -0400

    Added async and Promises

--- app.js
+++ app.js
@@ -1,39 +1,163 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template");
+const inquirer = require("inquirer");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 
-// // .slice() returns a brand-new array based on process.argv starting at the third index and ending with final index.
-// Array which holds the user command-line arguments.
-// Want to extract those arguments and store them into distinct variables.
-const profileDataArgs = process.argv.slice(2);
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name? (Required)",
+      //Uses method validate
+      //Receives argument of nameInput
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    //Ask user if they'd like to put in an about section.
+    //Confirmation question.
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
+    },
+    //'when' property; passes object of all the answers given so far as an object
+    //Will only be prompted if the answer to above is true.
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => confirmAbout,
+    },
+  ]);
+};
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+// In order to store multiple objects, have function accept argument and add project data to the argument; with the argument, we can add project data to the argument variable and then call the function with the modified data.
+const promptProject = (portfolioData) => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
 
-// Assignment destructuring - assigns elements of an array to variable names in a single expression:
-const [name, github] = profileDataArgs;
+  // If there's no 'projects' array property, create one.
+  if (!portfolioData.projects) {
+    // Added projects array to the object and initialized it as an empty array.
+    // Set to occur on the first pass only.
+    portfolioData.projects = [];
+  }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project description!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you this project with? (Check all that apply)",
+        choices: [
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Enter the GitHub link to your project. (Required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log("You need to enter a project GitHub link!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "Would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((projectData) => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
 
-// const printProfileData = (profileDataArr) => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log("================");
-
-//   // Is the same as this...
-//   profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-// Uses template literals to insert the variables inside the function block.
-// Interpolate the variables with the ${syntax}.
-
-// Will create an HTML file.
-// First argument is file name that will be created/the output file.
-// Second argument is the data that's being written: the HTML string template.
-// Third argument is the callback function that will handle errors as well as the success message.
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+promptUser()
+  .then(promptProject)
+  .then((portfolioData) => {
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
